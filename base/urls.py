d62490470b781cf811188007be1f236936bdf3fd
yoshinoseiji <sij.ysn25@gmail.com>
2021-06-03 03:44:30 +0900

    Add registerUser

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
   path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('users/register/', views.registerUser, name='user_register'),
   path('messages/send/', views.sendMessage, name='message_send'),
   path('messages/', views.getMessages, name='messages_get'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.hashers import make_password
 
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -30,13 +30,31 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 @api_view(['POST'])
+def registerUser(request):
+  data = request.data
+
+  is_registered = User.objects.filter(username=data['username']).exists()
+  if is_registered:
+    content = {'detail': 'このユーザーは既に存在しています'}
+    return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+  user = User.objects.create(
+    username = data['username'],
+    password = make_password(data['password']),
+  )
+
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
+
+
+@api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def sendMessage(request):
   user = request.user
   data = request.data
 
   message = Message.objects.create(
-    user_id=user,
+    user_id = user,
     text = data['text']
   )
 
