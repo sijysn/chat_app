8978abe0474e33c46284e101625c26c810fca32b
yoshinoseiji <sij.ysn25@gmail.com>
2021-06-03 03:47:48 +0900

    Add register

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,6 +5,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../consts/userConsts";
 
 export const login = (username, password) => async (dispatch) => {
@@ -30,7 +33,10 @@ export const login = (username, password) => async (dispatch) => {
 
     localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
-    dispatch({ type: USER_LOGIN_FAIL, payload: error });
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload: "ユーザーネームまたはパスワードが間違っています",
+    });
   }
 };
 
@@ -38,4 +44,35 @@ export const logout = () => (dispatch) => {
   dispatch({ type: USER_LOGOUT });
 
   localStorage.removeItem("userInfo");
+};
+
+export const register = (username, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/register/",
+      {
+        username: username,
+        password: password,
+      },
+      config
+    );
+
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
 };
