ccd03372e5ec2576b5e983365f4e572e38911fc0
yoshinoseiji <sij.ysn25@gmail.com>
2021-06-03 03:48:29 +0900

    Update

--- frontend/src/reducers/reducer.js
+++ frontend/src/reducers/reducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 
-import { userLoginReducer } from "./userReducers";
+import { userLoginReducer, userRegisterReducer } from "./userReducers";
 import { messageListReducer, messageSendReducer } from "./messageReducers";
 
 const reducer = combineReducers({
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
   messageList: messageListReducer,
   messageSend: messageSendReducer,
 });
